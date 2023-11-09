f2cccd101841fdfaa2e3b6e790951c7ec8a896b4
Clara Moulton <claramoulton@Claras-MBP.lan>
2023-11-09 11:28:58 -0500

    random numbers

--- sandbox/random_repo/script.js
+++ sandbox/random_repo/script.js
@@ -1,1 +1,36 @@
-console log 
+function countSevens(arr){
+    let sevens = 0;
+    for(let i = 0; i < arr.length; i = i +1){
+        if(arr[i] == 7){
+            sevens = sevens +1;
+        }
+    }
+    return sevens;
+}
+
+function createRandoms(limit){
+    const numbers = {};
+    return numbers;
+}
+
+const numbers = [];
+
+while(numbers.length < 100){
+    const num = Math.ceil(Math.random()*10);
+    numbers.push(num);
+}
+
+/*
+let sevens = 0;
+for(let i = 0; i < numbers.length; i = i + 1){
+    if(numbers[i] === 7){
+        sevens = sevens + 1
+    }
+}
+
+console.log(sevens);
+*/
+
+console.log(createRandoms(3));
+//console.log(countSevens([]));
+//console.log(countSevens(numbers));
